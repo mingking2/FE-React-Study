9d3a610155ee5dae45be98038f66561e0fc834e5
hotteok <hojun7889@gmail.com>
2023-07-23 22:36:17 +0900

    Fix: fixed by comment on my pull request

--- week2/hojun/Timer2.js
+++ week2/hojun/Timer2.js
@@ -1,98 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from "react";
 
-import { AiOutlinePlayCircle, AiOutlineReload } from 'react-icons/ai';
+import { AiOutlinePlayCircle, AiOutlineReload } from "react-icons/ai";
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import { Progress } from 'react-sweet-progress';
-import 'react-sweet-progress/lib/style.css';
+import { Progress } from "react-sweet-progress";
+import "react-sweet-progress/lib/style.css";
 
-import styled from 'styled-components';
-
-const Timer2 = () => {
-    const [seconds, setSeconds] = useState(60);
-    const [inputSecond, setInputSecond] = useState(60);
-    const [timerActive, setTimerActive] = useState(false);
-    const [error, setError] = useState(false);
-
-    const intervalRef = useRef();
-
-    const handleStart = () => {
-        setTimerActive(true);
-        setError(false);
-        toast.success('Timer started!');
-    };
-
-    const handleReset = () => {
-        setTimerActive(false);
-        setSeconds(inputSecond);
-        setError(false);
-        toast.info('Timer reset!');
-    };
-
-    useEffect(() => {
-        if (timerActive) {
-            intervalRef.current = setInterval(() => {
-                setSeconds((prevSeconds) => {
-                    if (prevSeconds === 0) {
-                        clearInterval(intervalRef.current);
-                        setTimerActive(false);
-                        toast.warn('Timer finished!');
-                        return inputSecond;
-                    }
-                    else return prevSeconds - 1;
-                });
-            }, 1000);
-        } else clearInterval(intervalRef.current);
-
-        return () => clearInterval(intervalRef.current);
-    }, [timerActive, inputSecond]);
-
-    const handleTimeChange = (event) => {
-        const newTime = parseInt(event.target.value, 10);
-
-        if (Number.isInteger(newTime) && newTime >= 0) {
-            setSeconds(newTime);
-            setInputSecond(newTime);
-            setError(false);
-        } else {
-            if (Number.isNaN(newTime)) {
-                setInputSecond('');
-                setSeconds(0);
-            }
-            setError(true);
-        }
-    };
-
-    return (
-        <Container>
-            <h1>Week_2</h1>
-            <h2>Timer: {seconds} seconds</h2>
-            <StyledProgress
-                type="circle"
-                width={70}
-                percent={(seconds / inputSecond * 100).toFixed()}
-            />
-            <ErrorMessage>{error && 'Please enter a valid positive integer for time.'}</ErrorMessage>
-            {!timerActive ? (
-                <InputContainer>
-                    <input type="number" value={inputSecond} onChange={handleTimeChange} />
-                    <StartButton onClick={handleStart}>
-                        <AiOutlinePlayCircle /> Start
-                    </StartButton>
-                </InputContainer>
-            ) : (
-                <ResetButton onClick={handleReset}>
-                    <AiOutlineReload /> Reset
-                </ResetButton>
-            )}
-            <ToastContainer position="bottom-right" />
-        </Container>
-    );
-};
-
-export default Timer2;
+import styled from "styled-components";
 
 const Container = styled.div`
   text-align: center;
@@ -141,3 +57,96 @@ const ResetButton = styled.button`
   cursor: pointer;
   margin-top: 20px;
 `;
+
+const Timer2 = () => {
+  const [seconds, setSeconds] = useState(60);
+  const [inputSecond, setInputSecond] = useState(60);
+  const [isActive, setIsActive] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const intervalRef = useRef();
+
+  const handleStart = () => {
+    setIsActive(true);
+    setIsError(false);
+    toast.success("Timer started!");
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setSeconds(inputSecond);
+    setIsError(false);
+    toast.info("Timer reset!");
+  };
+
+  useEffect(() => {
+    if (isActive) {
+      intervalRef.current = setInterval(() => {
+        setSeconds((prevSeconds) => {
+          if (prevSeconds === 0) {
+            clearInterval(intervalRef.current);
+            setIsActive(false);
+            toast.warn("Timer finished!");
+            return inputSecond;
+          } else return prevSeconds - 1;
+        });
+      }, 1000);
+    } else clearInterval(intervalRef.current);
+
+    return () => clearInterval(intervalRef.current);
+  }, [isActive, inputSecond]);
+
+  const handleTimeChange = (event) => {
+    const newTime = parseInt(event.target.value, 10);
+
+    if (Number.isInteger(newTime) && newTime >= 0) {
+      setSeconds(newTime);
+      setInputSecond(newTime);
+      setIsError(false);
+    } else {
+      if (Number.isNaN(newTime)) {
+        setInputSecond("");
+        setSeconds(0);
+      }
+      setIsError(true);
+    }
+  };
+
+  return (
+    <Container>
+      <h1>Week_2</h1>
+      <h2>Timer: {seconds} seconds</h2>
+      <StyledProgress
+        type="circle"
+        width={70}
+        percent={
+          Number.isNan(((seconds / inputSecond) * 100).toFixed()) === true
+            ? 0
+            : ((seconds / inputSecond) * 100).toFixed()
+        }
+      />
+      <ErrorMessage>
+        {isError && "Please enter a valid positive integer for time."}
+      </ErrorMessage>
+      {!isActive ? (
+        <InputContainer>
+          <input
+            type="number"
+            value={inputSecond}
+            onChange={handleTimeChange}
+          />
+          <StartButton onClick={handleStart}>
+            <AiOutlinePlayCircle /> Start
+          </StartButton>
+        </InputContainer>
+      ) : (
+        <ResetButton onClick={handleReset}>
+          <AiOutlineReload /> Reset
+        </ResetButton>
+      )}
+      <ToastContainer position="bottom-right" />
+    </Container>
+  );
+};
+
+export default Timer2;
