77b72cf8b3688a6838229caf9b08755fdd317cbe
whitecity01 <kh081104@gmail.com>
2023-07-31 13:46:28 +0900

    [Add] 비밀번호 조건 추가

--- week3/seung je/UseFormValidation.js
+++ week3/seung je/UseFormValidation.js
@@ -22,6 +22,16 @@ const UseFormValidation = () => {
       value: 8,
       message: "8자리 이상 비밀번호를 사용하세요.",
     },
+    validate: {
+      // 비밀번호에 숫자, 영어 소문자, 특수문자가 각각 하나 이상 포함되어야 함
+      hasNumber: (value) =>
+        /\d/.test(value) || "숫자를 최소 한 개 이상 포함해야 합니다.",
+      hasChar: (value) =>
+        /[a-zA-Z]/.test(value) || "영문자를 최소 한 개 이상 포함해야 합니다.",
+      hasSpecialChar: (value) =>
+        /[^A-Za-z0-9]/.test(value) ||
+        "특수문자를 최소 한 개 이상 포함해야 합니다.",
+    },
   };
 
   return {
