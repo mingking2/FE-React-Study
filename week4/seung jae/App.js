3d28eeb500a1125f3563750cd9f94c1676681a77
whitecity01 <kh081104@gmail.com>
2023-08-08 04:25:41 +0900

    [Edit] 가독성 및 중복 제거

--- week4/seung jae/App.js
+++ week4/seung jae/App.js
@@ -2,14 +2,20 @@ import { useState, useRef, useCallback, useEffect } from 'react';
 import TodoTemplate from './compoent/TodoTemplate';
 import TodoInsert from './compoent/TodoInsert';
 import TodoList from './compoent/TodoList';
-import firebase from './firebase';
+import firebase from './firebaseApi';
 
 const App = () => {
-  useEffect(() =>{ //제일 처음 db와 동기화, 스냅샷 저장, id값 불러오기
-    firebase.updateData(setTodos, (value) => nextId.current = value);
-  },[]);
   const [todos, setTodos] = useState([]);
   const nextId = useRef(1);
+  
+  useEffect(() => { //제일 처음 db와 동기화, 스냅샷 저장, id값 불러오기
+    firebase.updateData((todoList) => {
+      setTodos(todoList);
+      todoList.forEach((todo) => {
+        if (todo.id >= nextId.current) nextId.current = todo.id + 1;
+      }) 
+    })
+  }, []);
 
   const onInsert = useCallback((text) => {  //todo 추가
     const todo = {
@@ -19,21 +25,21 @@ const App = () => {
     };
 
     firebase.addData(todo);
-  },[]);
+  }, []);
 
   const onRemove = useCallback((id) => {  //todo 삭제
-    setTodos(todos.filter((todo) => {
+    setTodos( todos.filter((todo) => {
       if (todo.id === id) {
-        firebase.removeData(todo);  //해당하는 id의 todo 삭제
+        firebase.removeData(todo); //해당하는 id의 todo 삭제
         return false;
       } else return true;
     }))
   },[todos]);
 
-  const onToggle = useCallback((id)=> {   //todo 변경(checked)
-    setTodos( todos.map(todo =>{
-      if(todo.id === id){
-        const tmp = { ...todo, checked: !todo.checked};
+  const onToggle = useCallback((id) => {  //todo 변경(checked)
+    setTodos( todos.map((todo) => {
+      if (todo.id === id) {
+        const tmp = { ...todo, checked: !todo.checked };
         firebase.editData(tmp);
         return tmp;
       } else return todo;
@@ -44,7 +50,7 @@ const App = () => {
     <div>
       <TodoTemplate>
         <TodoInsert onInsert={onInsert} />
-        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
       </TodoTemplate>
     </div>
   );
