f12042f85295c90432cfa31e5ddca5f2db64d324
whitecity01 <kh081104@gmail.com>
2023-08-13 17:41:36 +0900

    [Edit] 코드 가독성 수정 및 버그 수정

--- week5/seung je/compoent/Todo/TodoInsert.js
+++ week5/seung je/compoent/Todo/TodoInsert.js
@@ -1,27 +1,32 @@
-import { useCallback, useState } from 'react';
-import { MdAdd } from 'react-icons/md';
-import './TodoInsert.scss';
+import { useCallback, useState } from "react";
+import { MdAdd } from "react-icons/md";
+import "./TodoInsert.scss";
 
-const TodoInsert = ({onInsert}) => {
-    const [value, setValue] = useState('');
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState("");
 
-    const onChange = useCallback(e => {
-        setValue(e.target.value);
-    }, []);
-  
-    const onSubmit = useCallback(
-        e => {
-            onInsert(value);
-            setValue('');
-            e.preventDefault();
-        },
-        [onInsert,value],
-    );
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
 
+  const onSubmit = useCallback(
+    (e) => {
+      if (value !== "") {
+        onInsert(value);
+        setValue("");
+      }
+      e.preventDefault();
+    },
+    [onInsert, value]
+  );
 
-    return (
+  return (
     <form className="TodoInsert" onSubmit={onSubmit}>
-      <input placeholder="할 일을 입력하세요" value={value} onChange={onChange} />
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
